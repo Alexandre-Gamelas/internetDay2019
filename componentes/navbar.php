f7559fd1475d5e4779bb8acbc9a8e9cd0dfc1627
J-Brandao <jp.brandao@ua.pt>
2019-04-24 17:55:50 +0100

    commit 3

--- componentes/navbar.php
+++ componentes/navbar.php
@@ -4,19 +4,15 @@
 <style>
 <?php include "css/navbar.css"?>
 </style>
-<nav class="navbar navbar-expand navbar-bg  pl-0 pr-0">
+<nav class="navbar navbar-expand navbar-bg  pl-0 pr-0 position-relative">
 
     <div class="navbar-layer-preto" ></div>
-
-
-
-
 
     <div class="collapse navbar-collapse" id="navbarsExample02">
         <a class="navbar-brand pl-4" href="#"> <img class="icon_back" src="assets/img/navbar/back-arrow.svg"></a>
         <ul class="navbar-nav m-auto">
-            <li class="nav-item active">
-                <a class="nav-link nav-texto" href="index.php"> <?php echo $pagina ?> </a>
+            <li class="nav-item active pos-center">
+                <a class="nav-link nav-texto" href="menuC.php"> <?php echo $pagina ?> </a>
             </li>
 
         </ul>
